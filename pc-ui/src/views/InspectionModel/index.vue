513b97d6cee0b323731b6ac58c350c10b44e0d3d
zrm <2637742003@qq.com>
2025-04-07 16:29:52 +0800

    检测模型管理

--- pc-ui/src/views/InspectionModel/index.vue
+++ pc-ui/src/views/InspectionModel/index.vue
@@ -1,32 +1,23 @@
 <template>
   <div>
     <el-form :model="queryParams" v-show="showSearch" ref="queryForm" size="small" :inline="true">
-      <el-row>
-        <el-col :span="8">
-          <el-form-item label="姓名" prop="name">
-            <el-input v-model="queryParams.name"
-                      placeholder="请输入姓名"
-                      clearable maxlength="50"
-                      style="width: 200px;"
-                      @keyup.enter.native="handleQuery"/>
-          </el-form-item>
-        </el-col>
-        <el-col :span="8">
-          <el-form-item label="工号" prop="engineerId">
-            <el-input v-model="queryParams.engineerId"
-                      placeholder="请输入工号"
-                      clearable maxlength="50"
-                      style="width: 200px;"
-                      @keyup.enter.native="handleQuery"/>
-          </el-form-item>
-        </el-col>
-        <el-col :span="8">
-          <el-form-item style="position: absolute; right: 1%">
-            <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
-            <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
-          </el-form-item>
-        </el-col>
-      </el-row>
+      <el-form-item label="检测模型名称" prop="modelId">
+        <el-select v-model="queryParams.modelId"
+                  placeholder="请输入检测模型名称"
+                  clearable maxlength="50"
+                  style="width: 200px;"
+                  @change="handleQuery">
+          <el-option v-for="item in modelList" :key="item.modelId"
+                     :value="item.modelId" :label="item.modelName" ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+            type="info"
+            plain
+            size="mini"
+            @click="handleViewModelImage">查看本模型图</el-button>
+      </el-form-item>
     </el-form>
 
     <el-row :gutter="10" style="margin-bottom: 8px">
@@ -36,8 +27,17 @@
             plain
             icon="el-icon-plus"
             size="mini"
-            @click="handleAdd"
-        >新增</el-button>
+            @click="handleAddModel"
+        >新增模型</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+            type="primary"
+            plain
+            icon="el-icon-plus"
+            size="mini"
+            @click="handleAddProject"
+        >新增项目</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -56,13 +56,18 @@
       <el-table-column label="序号" type="index" align="center" width="60">
         <template slot-scope="scope">{{(queryParams.pageNum-1)*queryParams.pageSize+scope.$index+1}}</template>
       </el-table-column>
-      <el-table-column label="工号" align="center" prop="engineerId" />
-      <el-table-column label="姓名" align="center" prop="name" />
-      <el-table-column label="性别" align="center" prop="gender" />
-      <el-table-column label="所属部门" align="center" prop="department" />
-      <el-table-column label="工龄" align="center" prop="workYear"/>
-      <el-table-column label="人脸图片" align="center" prop="faceInfo" />
-      <el-table-column label="联系方式" align="center" prop="contactInfo" />
+      <el-table-column label="检测项目编号" align="center" prop="projectId" />
+      <el-table-column label="检测项目名称" align="center" prop="projectName" />
+      <el-table-column label="项目点位" align="center" prop="projectPoints" />
+      <el-table-column label="描述" align="center" prop="description" />
+      <el-table-column label="标记情况" align="center" prop="tagStatus" width="100px">
+        <template slot-scope="scope">
+          <div :style="{borderRadius: '5px', backgroundColor: getTagBackColor(scope.row.tagStatus), color: getTagNameBackColor(scope.row.tagStatus)}">
+            {{ Boolean(scope.row.tagStatus) ? '是' : '否' }}
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column label="示意图" align="center" prop="referenceImages" />
       <el-table-column label="操作" align="center" fixed="right" width="180">
         <template slot-scope="scope">
           <el-button
@@ -77,6 +82,12 @@
               icon="el-icon-delete"
               @click="handleDelete(scope.row)"
           >删除</el-button>
+          <el-button
+              size="mini"
+              type="text"
+              icon="el-icon-edit"
+              @click="handleTag(scope.row)"
+          >标记点位</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -89,32 +100,44 @@
         @pagination="getList"
     />
 
+    <!--    新增模型弹窗-->
+    <el-dialog title="新增模型" :visible.sync="modalOpen" width="520px" append-to-body :close-on-click-modal="false">
+      <el-form ref="modelForm" :model="modelForm" :rules="rules" label-width="90px">
+        <el-form-item label="模型名称" prop="modalName">
+          <el-input v-model="modelForm.modalName" placeholder="请输入模型名称" />
+        </el-form-item>
+        <el-form-item label="对应机型" prop="modelAircraftTypeId">
+          <el-select v-model="modelForm.modelAircraftTypeId" placeholder="请选择对应飞机型号">
+            <el-option v-for="item in aircraftTypeList" :key="item.typeId"
+                       :value="item.typeId" :label="item.typeName"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="描述" prop="modelDescription">
+          <el-input v-model="modelForm.modelDescription" type="textarea" placeholder="请输入内容"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitModelForm">确 定</el-button>
+        <el-button @click="cancelAddModel">取 消</el-button>
+      </div>
+    </el-dialog>
     <!--    新增/修改弹窗-->
     <el-dialog :title="title" :visible.sync="open" width="520px" append-to-body :close-on-click-modal="false">
       <el-form ref="form" :model="form" :rules="rules" label-width="90px">
-        <el-form-item label="工号" prop="engineerId">
-          <el-input v-model="form.engineerId" placeholder="请输入工号" />
+        <el-form-item label="检测项目编号" prop="projectId">
+          <el-input v-model="form.projectId" placeholder="请输入检测项目编号" />
         </el-form-item>
-        <el-form-item label="姓名" prop="name">
-          <el-input v-model="form.name" placeholder="请输入姓名" />
+        <el-form-item label="检测项目名称" prop="projectName">
+          <el-input v-model="form.projectName" placeholder="请输入检测项目名称" />
         </el-form-item>
-        <el-form-item label="性别" prop="description">
-          <el-select v-model="form.description" placeholder="请选择性别">
-            <el-option v-for="item in genderList" :key="item.dictKey"
-                       :value="item.dictKey" :label="item.dictName"></el-option>
-          </el-select>
+        <el-form-item label="项目点位" prop="projectPoints">
+          <el-input v-model="form.projectPoints" placeholder="请输入项目点位"/>
         </el-form-item>
-        <el-form-item label="所属部门" prop="department">
-          <el-select v-model="form.department" placeholder="请选择所属部门">
-            <el-option v-for="item in departmentList" :key="item.dictKey"
-                       :value="item.dictKey" :label="item.dictName"></el-option>
-          </el-select>
+        <el-form-item label="描述" prop="description">
+          <el-input v-model="form.description" type="textarea" placeholder="请输入内容"/>
         </el-form-item>
-        <el-form-item label="人脸图片" prop="faceInfo">
-          <el-input v-model="form.faceInfo"/>
-        </el-form-item>
-        <el-form-item label="联系方式" prop="contactInfo">
-          <el-input v-model="form.contactInfo" placeholder="请输入联系方式" />
+        <el-form-item label="示意图" prop="referenceImages">
+          <el-input v-model="form.referenceImages"/>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -127,7 +150,7 @@
 
 <script>
 export default {
-  name: "DetectModel",
+  name: "InspectionModel",
   data() {
     return {
       // 遮罩层
@@ -145,28 +168,30 @@ export default {
       total: 3,
       // 字典列表
       dictList: [
-        { engineerId: '33467', name: 'xxx', gender: '女', department: '维修控制中心', faceInfo: '', contactInfo: '13323456472', workYear: '3年'},
-        { engineerId: '33468', name: 'admin', gender: '男', department: '航班维修部门', faceInfo: '', contactInfo: '13323456472', workYear: '3年'},
-        { engineerId: '33469', name: 'aaa', gender: '女', department: '航班维修部门', faceInfo: '', contactInfo: '13323456472', workYear: '2年'},
-        { engineerId: '33470', name: 'bbb', gender: '男', department: '维修控制中心', faceInfo: '', contactInfo: '13323456472', workYear: '1年'},
+        { projectId: '检测项目-1', projectName: '应急设备检查', projectPoints: '1', description: 'xxx', tagStatus: '1', referenceImages: ''},
+        { projectId: '检测项目-2', projectName: '起落架系统', projectPoints: '2', description: 'xxx', tagStatus: '0', referenceImages: ''},
+        { projectId: '检测项目-3', projectName: '驾驶舱面板', projectPoints: '3', description: 'xxx', tagStatus: '1', referenceImages: ''},
+        { projectId: '检测项目-4', projectName: '发动机系统', projectPoints: '4', description: 'xxx', tagStatus: '0', referenceImages: ''},
       ],
       //
-      genderList: [],
+      modelList: [],
       //
-      departmentList: [],
+      aircraftTypeList: [],
       // 查询参数
       queryParams: {
         pageNum: 1,
         pageSize: 10,
-        engineerId: null,
-        name: null,
-        gender: null,
-        department: null,
-        workYear: null,
-        contactInfo: null,
-        userId: null,
-        createdAt: null,
-        updatedAt: null
+        modelId: null,
+      },
+      modalOpen:false,
+      modelForm: {},
+      modelRules: {
+        modalName: [
+          { required: true, message: "模型名称不能为空", trigger: "blur" }
+        ],
+        modelAircraftTypeId: [
+          { required: true, message: "对应机型不能为空", trigger: "blur" }
+        ]
       },
       // 弹窗标题
       title: '',
@@ -200,10 +225,33 @@ export default {
       this.resetForm("queryForm");
       this.handleQuery();
     },
-    handleAdd(){
+    handleViewModelImage(){
+
+    },
+    handleAddModel(){
+      this.resetModelForm();
+      this.modalOpen = true;
+      this.title = "添加模型";
+    },
+    submitModelForm(){
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          // addType(this.form).then(response => {
+          this.$modal.msgSuccess("新增模型成功");
+          //   this.open = false;
+          //   this.getList();
+          // });
+        }
+      });
+    },
+    cancelAddModel(){
+      this.modalOpen = false;
+      this.resetModelForm();
+    },
+    handleAddProject(){
       this.reset();
       this.open = true;
-      this.title = "添加字典";
+      this.title = "添加项目";
     },
     handleUpdate(row){
       this.reset();
@@ -212,7 +260,7 @@ export default {
       //   this.form = response.data;
       this.form = row;
       this.open = true;
-      this.title = "修改字典类型";
+      this.title = "修改项目";
       // });
     },
     /** 提交按钮 */
@@ -271,6 +319,38 @@ export default {
       };
       this.resetForm("form");
     },
+    // 表单重置
+    resetModelForm() {
+      this.modelForm = {
+        modelId: undefined,
+        modelName: undefined,
+        modelAircraftTypeId: undefined,
+        modelDescription: undefined
+      };
+      this.resetForm("modelForm");
+    },
+    //
+    handleTag(){
+
+    },
+    //
+    getTagBackColor(tagStatus){
+      switch (tagStatus) {
+        case '0':
+          return '#FFCCCC';
+        case '1':
+          return '#CCFFCC';
+      }
+    },
+    //
+    getTagNameBackColor(tagStatus){
+      switch (tagStatus) {
+        case '0':
+          return '#FF0033';
+        case '1':
+          return '#009933';
+      }
+    }
   }
 }
 </script>
