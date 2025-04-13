c89af03a5b27d98c1305ef1acc07863dea929ec2
RidiculousBuffal <3322946829@qq.com>
2025-04-13 12:05:59 +0800

    添加配置

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -18,17 +19,26 @@ class Config:
     )
     JWT_SECRET_KEY = os.environ.get('SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)  # 1 天
-    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30) # refresh token 30天
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)  # refresh token 30天
+    # IPFS配置
+    IPFS_API_HOST = os.environ.get('ipfs_host') or 'http://127.0.0.1'
+    IPFS_API_PORT = int(os.environ.get('ipfs_port') or 10503)
+    IPFS_GATEWAY_HOST = os.environ.get('ipfs_gateway') or 'http://127.0.0.1'
+    IPFS_GATEWAY_PORT = int(os.environ.get('ipfs_gateway_port') or 8080)
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
 
+
 class ProductionConfig(Config):
     DEBUG = False
+
 
 config = {
     'development': DevelopmentConfig,
