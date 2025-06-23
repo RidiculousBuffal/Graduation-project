83a4da5bbc4ca2e7c527749f97defac4c9a800f1
RidiculousBuffal <3322946829@qq.com>
2025-06-23 22:10:23 +0800

    8 thread is enough

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ if [ "$MODE" = "api" ]; then
     exec gunicorn -k gevent -w 4 -b 0.0.0.0:5000 run:app
 elif [ "$MODE" = "worker" ]; then
     # 启动 celery worker
-    exec celery -A app.celery worker --beat --loglevel=info
+    exec celery -A app.celery worker --beat --loglevel=info -c 8
 else
     echo "MODE 参数必须为 'api'、'worker' 之一！当前值: ${MODE}"
     exit 1
