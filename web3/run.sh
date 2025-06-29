a9bf1d22394028a3ffbd15098a912174cc141d6d
RidiculousBuffal <3322946829@qq.com>
2025-06-29 13:18:37 +0000

    expose network

--- web3/run.sh
+++ web3/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-npx hardhat node &
+npx hardhat node --network hardhat --hostname 0.0.0.0 --port 8545&
 sleep 5
 npx hardhat ignition deploy ./ignition/modules/OperationLogger.ts --network hardhat
 tail -f /dev/null
