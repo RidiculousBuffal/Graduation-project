7d1dac34ace09a0d4632c02aa109f9598af59462
RidiculousBuffal <3322946829@qq.com>
2025-06-29 12:36:21 +0000

    fix build

--- web3/run.sh
+++ web3/run.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-
-# 启动 hardhat node 到后台
 npx hardhat node &
-
-# 等待端口 8545 启动（可自定义时间或用 npx wait-port 之类的工具更优雅）
-sleep 6
-
-# 部署报名
+sleep 5
 npx hardhat ignition deploy ./ignition/modules/OperationLogger.ts --network hardhat
-
-# 用 tail 阻塞，防止容器退出（可选，方便查看日志）
 tail -f /dev/null
