928ed455ae1bba6389a734469963d1b73a4939a5
RidiculousBuffal <3322946829@qq.com>
2025-06-29 11:38:30 +0000

    公网部署价格,延迟太高 平时用hardhat

---
+++ web3/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# 启动 hardhat node 到后台
+npx hardhat node &
+
+# 等待端口 8545 启动（可自定义时间或用 npx wait-port 之类的工具更优雅）
+sleep 6
+
+# 部署报名
+npx hardhat ignition deploy ./ignition/modules/OperationLogger.ts --network localhost
+
+# 用 tail 阻塞，防止容器退出（可选，方便查看日志）
+tail -f /dev/null
